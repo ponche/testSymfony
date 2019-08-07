46e8f5c76c699ca9c346a3b0591f7b9fb66b2750
Rémi <ponche62880@gmail.com>
2019-08-07 19:03:09 +0200

    test flash message in Symfony

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -9,14 +9,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class LuckyController extends AbstractController 
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number/{max}", name="app_lucky_number")
      */
-    public function number()
+    public function number($max)
     {
-        $number = random_int(0, 100); 
+        $number = random_int(0, $max); 
+        $test = $this->generateUrl('app_lucky_number', ['max' => 10]) ; 
+
+        // test flash message 
+        $this->addFlash('notice', 'Hello my rabbit') ; 
+        $this->addFlash('warning', 'SOS invasion rabbit') ; 
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number, 
+            'test' => $test, 
         ]); 
     }
 }
